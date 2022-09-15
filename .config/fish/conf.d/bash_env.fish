560bc9e46f6560dc5fbf1599f85cf1c8a3c26e11
AXON <axonasif@gmail.com>
2022-09-16 03:38:58 +0600

    Update bash_env.fish

--- .config/fish/conf.d/bash_env.fish
+++ .config/fish/conf.d/bash_env.fish
@@ -1,8 +1,10 @@
 #!/usr/bin/fish
 
-set blacklist_regex '^(SHLVL|PWD|SHELL|PS1|OLDPWD|LS_COLORS|BASH_FUNC.*|_)<<;``;>>';
-for value in (bash -c 'compgen -e | while read -r var; do \echo -n "${var}<<;\`\`;>>${!var}<<;##;>>"; done' | string split '<<;##;>>')
+set -l var_split '<<;@@;>>'
+set -l value_split '<<;##;>>'
+set blacklist_regex "^(SHLVL|PWD|SHELL|PS1|OLDPWD|LS_COLORS|BASH_FUNC.*|_)$var_split";
+for value in (bash -cli "compgen -e | while read -r var; do \echo -n \"\${var}$var_split\${!var}$value_split\"; done" | string split -n $value_split)
 	if string match --quiet --regex --invert $blacklist_regex $value;
-		set -gx (string split "<<;``;>>" value)
+		set -gx (string split $var_split $value)
 	end
 end
