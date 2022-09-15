f8b9ed96f8defcf7e316681d775c6286fd45e97b
AXON <axonasif@gmail.com>
2022-09-16 03:02:13 +0600

    Update bash_env.fish

--- .config/fish/conf.d/bash_env.fish
+++ .config/fish/conf.d/bash_env.fish
@@ -1,9 +1,8 @@
 #!/usr/bin/fish
 
-set blacklist_regex '^(SHLVL|PWD|SHELL|PS1|OLDPWD|LS_COLORS|BASH_FUNC.*|_)';
-
-bash -lc env | while read -d = -l name value
-	if string match --quiet --regex --invert "$blacklist_regex" "$name";
-		set -gx "$name" "$value";
+set blacklist_regex '^(SHLVL|PWD|SHELL|PS1|OLDPWD|LS_COLORS|BASH_FUNC.*|_)<<;``;>>';
+for value in (bash -c 'compgen -e | while read -r var; do \echo -n "${var}<<;\`\`;>>${!var}<<;##;>>"; done' | string split '<<;##;>>')
+	if string match --quiet --regex --invert $blacklist_regex $value;
+		set -gx (string split "<<;``;>>" value)
 	end
 end
