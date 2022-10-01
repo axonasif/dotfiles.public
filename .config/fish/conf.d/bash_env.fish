2fde2cdc5a91126e92bc0ed7cc6435f20fb6eb42
AXON <axonasif@gmail.com>
2022-10-01 08:05:13 +0000

    fix: bash_env, suppress erors from bash

--- .config/fish/conf.d/bash_env.fish
+++ .config/fish/conf.d/bash_env.fish
@@ -1,16 +1,16 @@
 #!/usr/bin/fish
 
 if status is-interactive
-	set bash_args '-cli'
+    set bash_args '-cli'
 else
-	set bash_args '-cl'
+    set bash_args '-cl'
 end
 
 set -l var_split '<<;@@;>>'
 set -l value_split '<<;##;>>'
-set blacklist_regex "^(SHLVL|PWD|SHELL|PS1|OLDPWD|LS_COLORS|BASH_FUNC.*|_)$var_split";
+set blacklist_regex "^(SHLVL|PWD|SHELL|PS1|OLDPWD|LS_COLORS|BASH_FUNC.*|_)$var_split"
 for value in (bash $bash_args "compgen -e | while read -r var; do \echo -n \"\${var}$var_split\${!var}$value_split\"; done" | string split -n $value_split)
-	if string match --quiet --regex --invert $blacklist_regex $value;
-		set -gx (string split $var_split $value)
-	end
+    if string match --quiet --regex --invert $blacklist_regex $value
+        set -gx (string split $var_split $value) 2>/dev/null || true
+    end
 end
