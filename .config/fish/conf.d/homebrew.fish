b3830a19cfc6a58f881d0f1b2ffed94cc322811c
AXON <axonasif@gmail.com>
2022-12-02 22:52:25 +0600

    fix homebrew.fish

--- .config/fish/conf.d/homebrew.fish
+++ .config/fish/conf.d/homebrew.fish
@@ -1,5 +1,11 @@
 if test -n "$HOMEBREW_PREFIX"
+
   set -a fish_function_path "$HOMEBREW_PREFIX/share/fish/vendor_functions.d"
   set -a fish_complete_path "$HOMEBREW_PREFIX/share/fish/vendor_completions.d"
-  source "${HOMEBREW_PREFIX}/share/fish/vendor_conf.d"/*
+
+  for f in "$HOMEBREW_PREFIX/share/fish/vendor_conf.d"/*
+    source "$f"
+  end
+  set -u f
+
 end
