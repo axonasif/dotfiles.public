bf365a96235fd06cd63c06aed698105d5af8bc83
Palanikannan1437 <73993394+Palanikannan1437@users.noreply.github.com>
2023-04-21 18:23:14 +0530

    tmux symlinked

--- .config/nvim/after/plugin/keymap/lazygit.lua
+++ .config/nvim/after/plugin/keymap/lazygit.lua
@@ -1,1 +1,1 @@
-vim.api.nvim_set_keymap("n", "<leader>h", "<cmd>LazyGit<CR>", { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap("n", "<leader>h", "<cmd>LazyGit<CR>", { noremap = true, silent = true })
