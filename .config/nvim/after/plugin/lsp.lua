d476c6c7d5404bd9e110c8337879491b4e89600a
Palanikannan1437 <73993394+Palanikannan1437@users.noreply.github.com>
2023-04-26 15:29:12 +0530

    added jump to defn on attach for lsp

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -1,4 +1,4 @@
-local opts = { noremap = true, silent = true }
+local options = { noremap = true, silent = true }
 local luasnip = require 'luasnip'
 local cmp = require('cmp')
 local lsp = require('lsp-zero').preset({
@@ -8,18 +8,9 @@ local lsp = require('lsp-zero').preset({
   suggest_lsp_servers = true,
 })
 
-vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
-vim.keymap.set({ 'n', 'x' }, '<leader>i', function()
-  vim.lsp.buf.format({ async = false, timeout_ms = 10000 })
-end)
-
 local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
   ['<C-p>'] = cmp.mapping.select_prev_item(),
-  -- ['<C-n>'] = cmp.mapping.select_next_item(),
   ['<C-n>'] = function(fallback)
     if cmp.visible() then
       cmp.select_next_item()
@@ -55,5 +46,26 @@ vim.diagnostic.config({
   virtual_text = false,
   globals = { "vim" },
 })
+
+lsp.on_attach(function(client, bufnr)
+  local options = { buffer = bufnr, remap = false }
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, options)
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, options)
+  vim.keymap.set('n', '<leader>vws', vim.lsp.buf.workspace_symbol, options)
+  vim.keymap.set('n', '<leader>vd', vim.diagnostic.open_float, options)
+  vim.keymap.set('n', '[d', vim.diagnostic.goto_next, options)
+  vim.keymap.set('n', ']d', vim.diagnostic.goto_prev, options)
+  vim.keymap.set('n', '<leader>vca', vim.lsp.buf.code_action, options)
+  vim.keymap.set('n', '<leader>vrr', vim.lsp.buf.references, options)
+  vim.keymap.set('n', '<leader>vrn', vim.lsp.buf.rename, options)
+
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, options)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, options)
+  vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, options)
+  vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, options)
+  vim.keymap.set({ 'n', 'x' }, '<leader>i', function()
+    vim.lsp.buf.format({ async = false, timeout_ms = 10000 })
+  end)
+end)
 
 lsp.setup()
